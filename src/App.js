c27f4fb1cd1251666728b7931eb69c1e1950fec3
Rafael Nascimento <rafaelradan76@gmail.com>
2021-12-25 03:37:38 -0300

    Add all the code at MyGithub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import './AppStyle.scss'
+
+import Logo from './img/logo.png'
 
 function App() {
   return (
+    //Start site
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="top-bar">
+        <div className="logo">
+          <img className="logo-devmedia" src={Logo} alt="logo dev midea" />
+        </div>
+
+        <div className="menus">
+          <h3>
+            <a href="#">CADASTRAR NOTICIAS</a>
+          </h3>
+          <h3>
+            <a href="#">EXIBIR NOTICIAS</a>
+          </h3>
+          <input className="search" />
+
+          <div className="mobile-menu">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
       </header>
-    </div>
-  );
+
+      <div className="body">
+        <div className="content">
+          <div className="news">
+            <h3>This's the first news</h3>
+            <p>
+              We are looking for programmer Front-end, this one will works in
+              sites of clientes, updating the pages and trying makes it better.
+            </p>
+            <button>Acessar</button>
+          </div>
+
+          <div className="news">
+            <h3>This's the first news</h3>
+            <p>
+              We are looking for programmer Front-end, this one will works in
+              sites of clientes, updating the pages and trying makes it better.
+            </p>
+            <button>Acessar</button>
+          </div>
+
+          <div className="news">
+            <h3>This's the first news</h3>
+            <p>
+              We are looking for programmer Front-end, this one will works in
+              sites of clientes, updating the pages and trying makes it better.
+            </p>
+            <button>Acessar</button>
+          </div>
+        </div>
+      </div>
+
+      <footer>Dessenvolved by Rafael Nascimento</footer>
+    </div> //End site
+  )
 }
 
-export default App;
+export default App
